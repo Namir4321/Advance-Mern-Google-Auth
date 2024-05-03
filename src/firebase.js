15fe5edcf6965052bf59d6e4c3aa878e6628caa7
Namir4321 <97880446+Namir4321@users.noreply.github.com>
2024-05-03 17:17:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBCHq73F258bSOcwXy5_AhSNgF5zJiy43E",
-  authDomain: "otp-verify-3e43c.firebaseapp.com",
-  projectId: "otp-verify-3e43c",
-  storageBucket: "otp-verify-3e43c.appspot.com",
-  messagingSenderId: "1085310507705",
-  appId: "1:1085310507705:web:aa3685ea5201a75bd4d968",
-  measurementId: "G-WRB54C4CFC",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
